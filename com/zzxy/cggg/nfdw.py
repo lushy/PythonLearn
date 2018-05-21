dc77b92ace9cdd4da41d2fdd5c6cd178729eef48
lushy <31211482+lushy@users.noreply.github.com>
2018-05-21 23:13:48 +0800

    添加容错数量机制，处理因为界面排序问题导致部分历史公告排在前面的问题

--- com/zzxy/cggg/nfdw.py
+++ com/zzxy/cggg/nfdw.py
@@ -3,10 +3,13 @@
 
 # 南方电网网站公告爬虫，记录公告名称、URL、公告日期到数据库
 
+import re
+import time
 import urllib.request
+
 from bs4 import BeautifulSoup
 import pymysql
-import re
+
 
 # 代理头部信息
 hdrs = {'User-Agent':'Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)'}
@@ -31,7 +34,7 @@ else:
 print('分页条数共计:%d' % split_size)
 
 try:
-    store_flag = False  # 是否数据库已存储标记，如果遍历到已存储公告退出
+    max_repeat_count = 50  # 最大重复重复记录，处理因为部分公告排序前面导致数据库直接存在记录退出问题
     insert_count = 0  # 本次记录插入数据库条数
     
     print('连接到mysql服务器...')
@@ -43,8 +46,6 @@ try:
 
     # 逐页请求数据
     for i in range(1, split_size):
-        # 访问一页后休眠2秒
-        # time.sleep(2)
         url = 'http://www.bidding.csg.cn/dbsearch.jspx?pageNo=' + str(i) + '&q=&org=&types='
         print('开始提取第' + str(i) + '页')
         request = urllib.request.Request(url, headers=hdrs)
@@ -65,6 +66,7 @@ try:
             web_li_a = web_li.find('a')  # 公告a链接
             ggmc = web_li_a.get('title')  # 公告名称
             ggurl = web_li_a.get('href')  # 公告链接
+            
             # 检索数据库是否已存储记录
             query_cggg = ('select count(*) from Cggg where Ggurl = %s') 
             query_data_cggg = ('http://www.bidding.csg.cn' + ggurl)
@@ -83,15 +85,21 @@ try:
                     insert_count += 1
                     print('插入成功!')
             else:
-                    store_flag = True
-                    print('记录已存在，退出当前页循环！')
-                    break
-                
-        # 如果出现数据记录存在情况，退出分页遍历循环
-        if store_flag :
-            print('数据已存储，退出分页循环！')
+                    # 如果最大重复数量大于0，则继续遍历下一条
+                    if max_repeat_count > 0 :
+                        max_repeat_count -= 1
+                        continue
+                    else :
+                        print('达到容错上限，退出当前页记录循环！')
+                        break
+        # 如果重复记录超过上限，则退出分页遍历
+        if max_repeat_count == 0 :
+            print('达到容错上限，退出分页循环！')
             break
-    print('共插入记录：%d，爬取数据并插入mysql数据库完成...' % insert_count)
+        
+        # 访问一页后休眠2秒，避免请求过快对服务器造成压力
+        time.sleep(2)
+    print('共插入记录：%d\n爬取数据并插入mysql数据库完成...' % insert_count)
 except Exception as e:    
         print(e)
         # 错误回滚    
